bd73ce69b31eb29888194aca5e1dbfc1384b6d6f
Kapoor-0905 <500097111@stu.upes.ac.in>
2022-08-28 09:41:34 +0530

    Question 6 & 7 added

---
+++ Experiment 2 & 3/Q6/Grades.java
@@ -0,0 +1,27 @@
+/*6) Write a program to accept 10 student's marks in an array, arrange it into ascending order, 
+convert into the following grades and print marks and grades in the tabular form. 
+- Between 40 and 50 : PASS 
+- Between 51 and 75 : MERIT 
+- and above : DISTINCTION*/
+
+import java.util.*;
+
+public class Grades{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int[] grades = new int[10];
+        for(int i =0 ; i < grades.length; i++){
+            grades[i] = input.nextInt();
+            if(grades[i]  > 40 && grades[i] <=50){
+                System.out.println(grades[i]+" "+":"+" "+"PASS");
+            } else if (grades[i]> 51 && grades[i]<75){
+                System.out.println(grades[i]+" "+":"+" "+"MERIT");
+            } else if(grades[i]> 75 && grades[i]<=100){
+                System.out.println(grades[i]+" "+":"+" "+"DISTINCTION");
+            } else if(grades[i]<=40){
+                System.out.println(grades[i]+" "+":"+" "+"FAIL");
+            }
+        }
+        input.close();
+    }
+}
