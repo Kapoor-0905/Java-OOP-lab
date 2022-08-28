82bf78b4a4be226dac27a933baecafb423078c79
Kapoor-0905 <500097111@stu.upes.ac.in>
2022-08-28 22:49:09 +0530

    Experiment 2 & 3 completed..

--- Experiment 2 & 3/Q6/Grades.java
+++ Experiment 2 & 3/Q6/Grades.java
@@ -5,13 +5,15 @@ convert into the following grades and print marks and grades in the tabular form
 - and above : DISTINCTION*/
 
 import java.util.*;
-
 public class Grades{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int[] grades = new int[10];
         for(int i =0 ; i < grades.length; i++){
             grades[i] = input.nextInt();
+        }
+        Arrays.sort(grades);
+        for(int i = 0; i < grades.length; i++){
             if(grades[i]  > 40 && grades[i] <=50){
                 System.out.println(grades[i]+" "+":"+" "+"PASS");
             } else if (grades[i]> 51 && grades[i]<75){
