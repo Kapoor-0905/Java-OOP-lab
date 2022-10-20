2234fd8efea90d6a6deac4f051d5f5fe2c44ccc3
Kapoor-0905 <500097111@stu.upes.ac.in>
2022-10-20 12:00:49 +0530

    Question 4 added

---
+++ Experiment 5/Q4/DivMod.java
@@ -0,0 +1,25 @@
+/*Write a program to create an Interface having two methods division and modules. Create a class, which overrides these methods */
+
+interface DivModo{
+    public void division(int a, int b);
+    public void modules(int a, int b);
+}   
+
+class DivModClass implements DivModo{
+    @Override
+    public void division(int a, int b){
+        System.out.println("Division of "+a+" and "+b+" is "+(a/b));
+    }
+    @Override
+    public void modules(int a, int b){
+        System.out.println("Modules of "+a+" and "+b+" is "+(a%b));
+    }
+}
+
+public class DivMod {
+    public static void main(String[] args) {
+        DivModClass obj = new DivModClass();
+        obj.division(10, 5);
+        obj.modules(10, 5);
+    }
+}
